49821d6f188f7e044032699f72ca2dd367cde14f
unknown <soikatdey18@gmail.com>
2024-09-23 03:17:50 -0700

    implement login with google

--- src/main.jsx
+++ src/main.jsx
@@ -35,11 +35,11 @@ const router = createBrowserRouter([
       },
       {
         path:"/profile",
-        element:<Profile/>
+        element:<PrivateRoute><Profile/></PrivateRoute>
       },
       {
         path:"/member",
-        element:<Member/>
+        element:<PrivateRoute><Member/></PrivateRoute>
       }
     ],
   },
